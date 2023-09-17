5d5f594437e6766a18fefc61d2d6c74ddb60d55e
Wesed <dev.wfeduardo@gmail.com>
2023-09-17 11:55:15 -0300

    removendo o component newRelease

--- src/pages/Transactions/components/TransactionsContainer/table.tsx
+++ src/pages/Transactions/components/TransactionsContainer/table.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
-import { NewRelease } from './newRelease'
 import { TransactionsContext } from '@/contexts/transactionsContext'
+import { FormatValue } from '@/components/utils/formatPrice'
 
 export function TableTransactions() {
   const { dataTransactions } = useContext(TransactionsContext)
@@ -9,14 +9,22 @@ export function TableTransactions() {
     <table className="w-full border-separate border-spacing-x-0 border-spacing-y-2">
       <tbody>
         {dataTransactions?.map((transaction) => (
-          <NewRelease
-            key={transaction.id}
-            description={transaction.description}
-            value={transaction.price}
-            category={transaction.category}
-            type={transaction.type}
-            createdAt={transaction.createdAt}
-          />
+          <tr key={transaction.id}>
+            <td className="column-table w-1/2">{transaction.description}</td>
+            <td className="column-table">
+              {transaction.type === 'income' ? (
+                <span className="text-green_300">
+                  {FormatValue(transaction.price)}
+                </span>
+              ) : (
+                <span className="text-red_300">
+                  - {FormatValue(transaction.price)}
+                </span>
+              )}
+            </td>
+            <td className="column-table">{transaction.category}</td>
+            <td className="column-table">{transaction.createdAt}</td>
+          </tr>
         ))}
       </tbody>
     </table>
