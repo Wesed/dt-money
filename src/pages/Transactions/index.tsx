83de712f7beb4f1d6aef36a5e957139a3fb5f370
Wesed <dev.wfeduardo@gmail.com>
2023-09-17 11:58:50 -0300

    removendo o component index no transactionsContainer | motivo - nao ha necessidade

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,13 +1,18 @@
 import { Header } from './components/Header'
 import { Summary } from './components/Summary'
-import { TransactionsContainer } from './components/TransactionsContainer'
+import { SearchForm } from './components/TransactionsContainer/searchForm'
+import { TableTransactions } from './components/TransactionsContainer/table'
 
 export function Transactions() {
   return (
     <div className=" min-h-screen bg-gray_800 text-gray_100">
       <Header />
       <Summary />
-      <TransactionsContainer />
+
+      <div className="mx-auto my-16 flex max-w-maxWidth flex-col gap-6 p-6">
+        <SearchForm />
+        <TableTransactions />
+      </div>
     </div>
   )
 }
